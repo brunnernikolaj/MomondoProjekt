ba9cd50bd9e9f4597eb40fbddcfac5aa4e02800e
Casper Schultz <casper@Caspers-MacBook-Pro.local>
2015-12-02 15:00:30 +0100

    Airport entity

---
+++ src/java/entity/Airport.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author casper
+ */
+@Entity
+@Table(name = "Airports")
+@NamedQueries({
+    @NamedQuery(name = "AirportEntity.findAirportByIATA", query = "SELECT p FROM AirportEntity p WHERE p.IATAcode = :IATAcode")})
+public class Airport {
+    
+    @Id
+    @Column(name = "AIRPORT_ID")
+    private int id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "CITY")
+    private String city;
+    @Column(name = "COUNTRY")
+    private String country;
+    @Column(name = "IATA_CODE")
+    private String IATAcode;
+    @Column(name = "ICAO_CODE")
+    private String ICAOcode;
+    @Column(name = "LATITUDE")
+    private String latitude;
+    @Column(name = "LONGITUDE")
+    private String longitude;
+    @Column(name = "ALTITUDE")
+    private String altitude;
+    @Column(name = "TIMEZONE")
+    private String timezone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getIATAcode() {
+        return IATAcode;
+    }
+
+    public void setIATAcode(String IATAcode) {
+        this.IATAcode = IATAcode;
+    }
+
+    public String getICAOcode() {
+        return ICAOcode;
+    }
+
+    public void setICAOcode(String ICAOcode) {
+        this.ICAOcode = ICAOcode;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(String altitude) {
+        this.altitude = altitude;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+}
