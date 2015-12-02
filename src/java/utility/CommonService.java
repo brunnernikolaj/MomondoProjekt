4750f267a806d603f71be173fc5c9e114ca00526
Casper Schultz <casper@Caspers-MacBook-Pro.local>
2015-12-02 10:40:24 +0100

    Added time to minutes service

--- src/java/utility/CommonService.java
+++ src/java/utility/CommonService.java
@@ -18,9 +18,23 @@ public class CommonService {
 	 */
 	public static int getRandomNumber(int low, int high) {
 		
-		Random r = new Random();
-		int result = r.nextInt(high-low) + low;
-		return result;
+            Random r = new Random();
+            int result = r.nextInt(high-low) + low;
+            return result;
 	}
+        
+        /**
+         * converts a java HH:mm into minutes.
+         * 
+         * @param time
+         * @return 
+         */
+        public static int timeToMins(String time) {
+            String[] hourMin = time.split(":");
+            int hour = Integer.parseInt(hourMin[0]);
+            int mins = Integer.parseInt(hourMin[1]);
+            int hoursInMins = hour * 60;
+            return hoursInMins + mins;
+        }
     
 }
