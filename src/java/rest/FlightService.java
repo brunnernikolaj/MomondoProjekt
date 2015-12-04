9412e5a59b2838f10c58f006703f96643e0ef299
Casper Schultz <casper@Caspers-MacBook-Pro.local>
2015-12-04 19:14:41 +0100

    Added caching

--- src/java/rest/FlightService.java
+++ src/java/rest/FlightService.java
@@ -16,14 +16,20 @@ import facades.FlightFacade;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import org.joda.time.DateTime;
 import utility.NorweigianDestinations;
 
@@ -32,7 +38,7 @@ import utility.NorweigianDestinations;
  *
  * @author casper
  */
-@Path("flightinfo")
+@Path("")
 public class FlightService {
 
     @Context
@@ -48,10 +54,35 @@ public class FlightService {
         gson = new GsonBuilder().setPrettyPrinting().create();
     }
     
+    @POST
+    @Path("flightreservation")
+    @Consumes(MediaType.APPLICATION_JSON) 
+    public String flightReservation() {
+        
+        
+        
+        return "";
+    }
+    
+    /**
+     * Returns flights from Just Fly (our) company.
+     * 
+     * Returns flights from a given origin but without destination.
+     * 
+     * @param request
+     * @Author: Casper Schultz
+     * @Date: 2/12 2015
+     * 
+     * @param from              Travel Origin as IATA code 
+     * @param day               Day as date string formatted as: 2016-02-25
+     * @param seats             Number of seats required
+     * @return                  Json object with flights that match the criteria.
+     * @throws FlightException  
+     */
     @GET
-    @Path("/{from}/{day}/{seats}")
+    @Path("/flightinfo/{from}/{day}/{seats}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getFlightsFrom(@PathParam("from") String from, @PathParam("day") String day, @PathParam("seats") int seats) throws FlightException {
+    public Response getFlightsFrom(@PathParam("from") String from, @PathParam("day") String day, @PathParam("seats") int seats, @Context Request request) throws FlightException {
         
         // We validate the input data, to make sure its a valid IATA code
         // and that the date has been formatted correctly
@@ -60,36 +91,27 @@ public class FlightService {
         
         // Fetch the flights
         List<Flight> flights = facade.getJFFlightsFrom(from, day, seats);
-
-        JsonArray jsonArray = new JsonArray(); 
-        JsonObject json = new JsonObject();
-        json.addProperty("airline", "Just Fly");
         
-        for (Flight flight : flights) {
-            
-            JsonObject obj = new JsonObject();
-            obj.addProperty("origin", flight.getIataFrom());
-            obj.addProperty("destination", flight.getIataTo());
-            obj.addProperty("flightNumber", flight.getFlightNumber());
-            obj.addProperty("noOfSeats", flight.getNoOfSeats());
-            obj.addProperty("travelTime", flight.getTravelTime());
-            obj.addProperty("price", flight.getPrice() * seats);
-            
-            DateTime date = new DateTime(flight.getTravelDate());
-            
-            obj.addProperty("travelDate", date.toString());
-            
-            jsonArray.add(obj);
+        // Add some caching
+        CacheControl cc = new CacheControl();
+        cc.setMaxAge(86400);
+        EntityTag etag = new EntityTag(Integer.toString(flights.hashCode()));
+        ResponseBuilder builder = request.evaluatePreconditions(etag);
+        
+        // cached resource did change -> serve updated content
+        if(builder == null) {
+            builder = Response.ok(buildJsonObject(flights, seats));
+            builder.tag(etag);
         }
-        
-        json.add("flights", jsonArray);
-        
-        return gson.toJson(json);
+
+        builder.cacheControl(cc);
+        return builder.build();
     }
     
     /**
      * Returns flights from Just Fly (our) company.
      * 
+     * @param request
      * @Author: Casper Schultz
      * @Date: 2/12 2015
      * 
@@ -98,13 +120,12 @@ public class FlightService {
      * @param day               Day as date string formatted as: 2016-02-25
      * @param seats             Number of seats required
      * @return                  Json object with flights that match the criteria.
-     * @throws ParseException
-     * @throws IOException 
+     * @throws FlightException  
      */
     @GET
-    @Path("/{from}/{to}/{day}/{seats}")
+    @Path("/flightinfo/{from}/{to}/{day}/{seats}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getFlightsToFrom(@PathParam("from") String from, @PathParam("to") String to, @PathParam("day") String day, @PathParam("seats") int seats) throws FlightException {
+    public Response getFlightsToFrom(@PathParam("from") String from, @PathParam("to") String to, @PathParam("day") String day, @PathParam("seats") int seats, @Context Request request) throws FlightException {
         
         // We validate the input data, to make sure its a valid IATA code
         // and that the date has been formatted correctly
@@ -115,11 +136,38 @@ public class FlightService {
         // Fetch the flights
         List<Flight> flights = facade.getJFFlights(from, to, day, seats);
         
-        // We need to conert the list to a json list in order to store it in the json object
-        //JsonElement element = gson.toJsonTree(flights, new TypeToken<List<Flight>>() {}.getType());
-        JsonArray jsonArray = new JsonArray(); //element.getAsJsonArray();
+         // Add some caching
+        CacheControl cc = new CacheControl();
+        cc.setMaxAge(86400);
+        EntityTag etag = new EntityTag(Integer.toString(flights.hashCode()));
+        ResponseBuilder builder = request.evaluatePreconditions(etag);
         
-        // We build the object
+        if(builder == null) {
+            builder = Response.ok(buildJsonObject(flights, seats));
+            builder.tag(etag);
+        }
+        
+        builder.cacheControl(cc);
+        return builder.build();
+    }
+    
+    
+    /**
+     * Builds Json object with flight results.
+     * 
+     * Builds a json object afther the specified format handed
+     * out in the assignment.
+     * 
+     * @Author: Casper Schultz
+     * @Date: 4/12 2015
+     * 
+     * @param flights       List of flight objects
+     * @param seats         The number of seats used to calculate the price
+     * @return              Json object as string
+     */
+    private String buildJsonObject(List<Flight> flights, int seats) {
+        
+        JsonArray jsonArray = new JsonArray(); 
         JsonObject json = new JsonObject();
         json.addProperty("airline", "Just Fly");
         
