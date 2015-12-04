36205aabf63db484c59c2fb8af286ad2755f49f7
Casper Schultz <casper@Caspers-MacBook-Pro.local>
2015-12-04 18:51:58 +0100

    Updated flight exception mapper

--- src/java/exceptions/FlightExceptionMapper.java
+++ src/java/exceptions/FlightExceptionMapper.java
@@ -6,8 +6,11 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 /**
- *
- * @author casper
+ * Exception mapper for the flight exeption described 
+ * in the assignment.
+ * 
+ * @Author: Casper Schultz
+ * @Date: 4/12 2015
  */
 @Provider
 public class FlightExceptionMapper implements ExceptionMapper<FlightException> {
@@ -18,15 +21,15 @@ public class FlightExceptionMapper implements ExceptionMapper<FlightException> {
      * @Author: Casper Schultz
      * @Date: 4/12 2015
      * 
-     * @param e
-     * @return 
+     * @param e         Exception that is getting mapped.
+     * @return          Response object with error as json in the body.
      */
     @Override
     public Response toResponse(FlightException e) {
         
         JsonObject json = new JsonObject();
         json.addProperty("httpError", e.getErrorCode());
-        json.addProperty("errorCode", Integer.parseInt(e.getStatusCode().toString()));
+        json.addProperty("errorCode", "" + e.getStatusCode().toString());
         json.addProperty("message", e.getMessage());
         return Response.status(e.getStatusCode()).entity(json.toString()).build();
     }
