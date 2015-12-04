7bcc63f69f8cbb5abdb8958f316f253d3ec0de02
Casper Schultz <casper@Caspers-MacBook-Pro.local>
2015-12-04 19:38:51 +0100

    Added passenger and reservation entity

---
+++ src/java/entity/Passenger.java
@@ -0,0 +1,84 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+/**
+ * Entity for storing a passenger.
+ * 
+ * @Author: Casper Schultz
+ * @Date: 4/12 2015
+ */
+@Entity
+@Table(name = "PASSENGERS")
+public class Passenger {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PASSENGER_ID")
+    private int id;
+    
+    @Column(name = "FIRSTNAME")
+    private String firstname;
+    
+    @Column(name = "LASTNAME")
+    private String lastname;
+    
+    @ManyToMany(mappedBy = "passengers", cascade = CascadeType.ALL)
+    @JoinColumn(name = "RESERVATION_ID")
+    private List<Reservation> reservations;
+    
+    public Passenger() {
+        reservations = new ArrayList<>();
+    }
+    
+    public void addReservation(Reservation reservation) {
+        reservations.add(reservation);
+    }
+    
+    public void removeReservation(Reservation reservation) {
+        if (reservations.contains(reservation)) 
+            reservations.remove(reservation);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+}
