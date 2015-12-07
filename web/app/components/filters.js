b8fcb617a38608e8c68312fdc97b6789d188371d
Mathias Holmbo Christensen <MathiasHC@Mathiass-MacBook-Pro.local>
2015-12-07 14:56:49 +0100

    Filter for travel time added to results page and filter created "traveltime" in filters.js file.

--- web/app/components/filters.js
+++ web/app/components/filters.js
@@ -2,9 +2,21 @@
 
 /* Place your Global Filters in this file */
 
-angular.module('myApp.filters', []).
-  filter('checkmark', function () {
-    return function(input) {
-      return input ? '\u2713' : '\u2718';
-    };
-  });
+angular.module('myApp.filters', [])
+	.filter('checkmark', function () {
+        return function(input) {
+          return input ? '\u2713' : '\u2718';
+        };
+    })
+    
+    .filter('traveltime', function() {
+        return function(input, traveltime) {
+            var h = Math.floor(input / 60); // Math.floo() runder ned
+            var m = (input - (h*60)); // træk timer fra minuttal
+
+            var time = h + 't ' + m + 'm';
+            console.log(input);
+            console.log(time);
+            return time;
+        };
+    });
